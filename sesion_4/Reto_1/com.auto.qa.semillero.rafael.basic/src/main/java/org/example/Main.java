7bb0f29ef901b14208b79b7b424811bfd98df58b
Rafael Londoño <43898423+rafael492@users.noreply.github.com>
2022-10-31 20:02:46 -0500

    se crea el repositorio
    creando y subiendo el contenido a github

---
+++ sesion_4/Reto_1/com.auto.qa.semillero.rafael.basic/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        String fecha;
+
+        fecha = "27/04/1992";
+        System.out.println("la fecha de nacimiento de Rafael es: " + fecha);
+    }
+
+}
